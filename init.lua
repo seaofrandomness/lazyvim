f8f77443eccf9a694c07b5a14ffb6465d9a901b7
seaofrandomness <125447188+seaofrandomness@users.noreply.github.com>
2024-09-07 16:50:17 +0100

    ensure gitsigns is loaded

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Ensure Gitsigns is loaded before running this
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    -- Enable current line blame by default
+    require('gitsigns').toggle_current_line_blame(true)
+  end
+}
